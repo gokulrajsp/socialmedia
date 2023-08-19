30e8f01fb1edc4ded8554d124db91037980ad23f
gokulrajsp <142140303+gokulrajsp@users.noreply.github.com>
2023-08-19 19:59:49 +0530

    Add files via upload

---
+++ routes/routes/users-routes.js
@@ -0,0 +1,18 @@
+import express from "express"
+import {getUser,getUserFriends,addRemoveFriend, updateUser, deleteUser} from "../controllers/users-controller.js"
+import {verifyToken} from "../middleware/auth-middleware.js"
+
+const router = express.Router()
+
+//Read
+router.get("/:id", verifyToken, getUser)
+router.get("/:id/friends", verifyToken, getUserFriends)
+
+//Update
+router.patch("/:id/:friendId", verifyToken,addRemoveFriend)
+router.patch("/:id", verifyToken, updateUser)
+
+//delete
+router.delete("/:id", verifyToken, deleteUser)
+
+export default router
